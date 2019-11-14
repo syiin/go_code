fa9307fb2013c129b60cbd755e96cd717296291a
syiin <xbio.oo@gmail.com>
2019-11-14 15:02:10 +0800

    annotated jwt authentication function

--- auth_db_api/src/utils/auth/auth.go
+++ auth_db_api/src/utils/auth/auth.go
@@ -4,6 +4,7 @@ import (
 	"models"
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -16,11 +17,12 @@ type Exception models.Exception
 // JwtVerify Middleware function
 func JwtVerify(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		var header = r.Header.Get("x-access-token") //Grab the token from the header
-
+		//1. Get the token string
+		//2. Create a key function to get the key
+		//3. Get the Claim used to sign the token string
+		//4. Give all 3 to the Parser to validate
+		var header = r.Header.Get("x-access-token") 
 		header = strings.TrimSpace(header)
-
 		if header == "" {
 			//Token is missing, returns with error code 403 Unauthorized
 			w.WriteHeader(http.StatusForbidden)
@@ -28,17 +30,27 @@ func JwtVerify(next http.Handler) http.Handler {
 			return
 		}
 		tk := &models.Token{}
+		keyFunc := func(token *jwt.Token) (interface{}, error) {
+					return []byte("secret"), nil }
 
-		_, err := jwt.ParseWithClaims(header, tk, func(token *jwt.Token) (interface{}, error) {
-			return []byte("secret"), nil
-		})
-
+		reconstructed, err := jwt.ParseWithClaims(header, tk, keyFunc)
 		if err != nil {
 			w.WriteHeader(http.StatusForbidden)
 			json.NewEncoder(w).Encode(Exception{Message: err.Error()})
 			return
 		}
 
+		//just to visualise the return struct
+		out, _ := json.Marshal(reconstructed) 
+		fmt.Println(string(out))
+
+		//a context is an interface, an interface is a description of method 
+		//signatures an object can have
+		//contexts define boundaries in the program (eg. stopping, channels)
+		//"user" is the key - WithValue() takes a context and creates a derivative
+		//context associated with the value (ie. tk)
+		//this is a way to pass on information - this information is local to
+		//the request and is then deleted once the request is done
 		ctx := context.WithValue(r.Context(), "user", tk)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
